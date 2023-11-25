5fcdecdad71009ef988d7a20343c069daf1e74a1
Harsh <harshmehra53@gmail.com>
2023-11-25 19:30:02 +0530

    up

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 'use strict';
 const API_key = '1d534a4580b33f86205c2a7927144055';
 
-async function showWeather() {
-  let city_name = 'jaipur';
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_key}`
-  );
-  const data = await response.json();
-  console.log('Weather data: ' + data);
+async function fetchWeatherDetails() {
+  try {
+    let city_name = 'jaipur';
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_key}`
+    );
 
-  let newPara = document.createElement('p');
-  newPara.innerHTML = `${data?.main?.temp.toFixed(2)} °C`;
+    if (!response.ok) {
+      throw new Error('New Error found');
+    }
+    const data = await response.json();
+    console.log('Weather data: ', data);
+    //--Creating new element and inserting new element into it
+    let newPara = document.createElement('p');
+    newPara.innerHTML = `${data?.main?.temp.toFixed(2)} °C`;
 
-  document.body.appendChild(newPara);
+    document.body.appendChild(newPara);
+  } catch (error) {
+    console.error('Error fetching weather data: ', error.message);
+  }
 }
